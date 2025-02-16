83ebe8a5538c4ebb981d433f55fa7342a49c0d00
Rita Kumari <amrita.spj2222@gmail.com>
2025-02-16 14:31:35 +0530

    Pratice & and solve the String Problem

--- String/concatination.cpp
+++ String/concatination.cpp
@@ -1,17 +1,26 @@
 #include<iostream>
+#include <cstring>
 using namespace std;
 
-int main(){
-    string str1, str2;
-    cout << "Enter two strings : ";
-    // getline(cin, str1, str2);
-    getline(cin, str1);
-    getline(cin, str2); // Ab clear hua
+// int main(){
+//     string str1, str2;
+//     cout << "Enter two strings : ";
+//     // getline(cin, str1, str2);
+//     getline(cin, str1);
+//     getline(cin, str2); // Ab clear hua
 
-    cout << "Concatinating strings are : " << str1 + str2 << endl;
-    return 0;
-}
-
-
+//     // cout << "Concatinating strings are : " << str1 +  " " + str2 << endl;
+//     cout << "Concatinating string are :" << str1.append(str2) << endl;
+//     return 0;
+// }
 // str1 = "Rita"; str2 = "Kumari";
 // Concatinating strings are : "Rita Kumari";
+
+int main(){
+    char str1[] = "Rita";
+    char str2[]= "Kumari";
+
+    //  String length
+    cout << "Length of the string is :" << strlen(str1) << endl;
+    return 0;
+}
